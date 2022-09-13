1c798d00e11973c77597b7286e0d5d4358e3083b
Pamir Perdeci <pamirperdeci@gmail.com>
2022-09-13 05:00:22 -0400

    9 tests passing

--- index.js
+++ index.js
@@ -44,6 +44,23 @@ class Airplane {
 */
 
 class Person {
+  constructor(name, age){
+    this.age = age;
+    this.name = name;
+    this.stomach = [];
+  }
+  eat(someFood) {
+    if(this.stomach.length < 10) {
+      this.stomach.push(someFood)
+    }
+
+  }
+  poop(){
+    this.stomach = [];
+  }
+  toString(){
+    return `${this.name}, ${this.age}`
+  }
   
 }
 
